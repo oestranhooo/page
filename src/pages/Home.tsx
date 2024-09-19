b8dc925b162f64f6e36e0ac56ba352cb2150084c
oestranhooo <matheusweiber2330@gmail.com>
2024-09-19 15:19:11 -0300

    commit atv

---
+++ src/pages/Home.tsx
@@ -0,0 +1,27 @@
+
+
+export default function Home (){
+    return(
+        <>
+        <header>
+         <h1>
+        TESTE!!!
+         </h1>
+         <h2>
+        TESTE!!!
+         </h2>
+         <h3>
+        TESTE!!!
+         </h3>
+         <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Soluções</a></li>
+            <li><a href="">Depoimentos</a></li>
+            <li><a href="">Preços</a></li>
+            <li><a href="">Contato</a></li>
+         </ul>
+         </header>
+    </>
+       
+    )
+}
---
+++ src/pages/header.tsx
@@ -0,0 +1,37 @@
+import Logo from "../assets/logo.svg";
+import "../styles/header.css";
+import "../styles/utility.css";
+
+export default function Home() {
+  return (
+    <>
+      <header className="container py-sm">
+        <nav className="flex items-center justify-between">
+          <img src={Logo} alt="Logo DonaFrost" width={220} height={80} />
+          <div className="desktop-only">
+            <ul className="flex gap-1">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#solution">Soluções</a>
+              </li>
+              <li>
+                <a href="#testimonials">Depoimentos</a>
+              </li>
+              <li>
+                <a href="#pricing">Preços</a>
+              </li>
+              <li>
+                <a href="#contact">Contato</a>
+              </li>
+            </ul>
+          </div>
+          
+          
+        </nav>
+        //Essa tag nav fecha lá em baixo com o header
+      </header>
+    </>
+  );
+}
